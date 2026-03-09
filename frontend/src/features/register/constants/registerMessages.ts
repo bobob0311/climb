f69c79b72310d60ac2249e6e79b28c61e3b64a2f
bosung <qweasdzxc0311@naver.com>
2026-03-09 23:32:18 +0900

    필요한 register 메시지 추가

--- frontend/src/features/register/constants/registerMessages.ts
+++ frontend/src/features/register/constants/registerMessages.ts
@@ -4,4 +4,7 @@ export const REGISTER_MESSAGES = {
     passwordRequired: '비밀번호를 입력해주세요.',
     passwordMismatch: '비밀번호와 비밀번호 확인이 일치하지 않습니다.',
     requiredCheckBox: '필수 체크 항목을 확인해주세요.',
+    loginIdAvailable: 'id가 중복되지 않습니다.',
+    nicknameAvailable: '닉네임이 중복되지 않습니다.',
+    signUpSuccess: '회원가입 되셨습니다.',
 } as const;
