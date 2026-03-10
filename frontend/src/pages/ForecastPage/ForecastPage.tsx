b13e8ae25934de5588745facafb69a43e9dddd1d
bosung <qweasdzxc0311@naver.com>
2026-03-10 20:57:09 +0900

    refactor: usePageSnapScroll 커스텀 훅 적용

--- frontend/src/pages/ForecastPage/ForecastPage.tsx
+++ frontend/src/pages/ForecastPage/ForecastPage.tsx
@@ -1,59 +1,13 @@
-import { useEffect } from 'react';
 import DetailInfoSection from '../../components/templates/Forecast/DetailInfoSection.tsx';
 import ForecastBackgroundSection from '../../components/templates/Forecast/ForecastBackgroundSection.tsx';
 import ForecastSearchSection from '../../components/templates/Forecast/ForecastSearchSection.tsx';
 import SummaryInfoSection from '../../components/templates/Forecast/SummaryInfoSection.tsx';
 import { css } from '@emotion/react';
 import { theme } from '../../theme/theme.ts';
+import usePageSnapScroll from '../../features/forecast/hooks/usePageSnapScroll.ts';
 
 export default function ForecastPage() {
-    useEffect(() => {
-        let isScrolling = false;
-
-        const handleWheel = (e: WheelEvent) => {
-            if (isScrolling) return;
-
-            const pageHeight = window.innerHeight;
-            const scrollY = window.scrollY;
-            const currentPage = Math.round(scrollY / pageHeight);
-
-            const offsetInPage = scrollY % pageHeight;
-            const downThreshold = 1;
-            const upThreshold = pageHeight - 1;
-
-            let targetPage = currentPage;
-
-            if (e.deltaY > 0) {
-                if (offsetInPage > downThreshold) {
-                    targetPage = currentPage + 1;
-                }
-            } else {
-                if (offsetInPage < upThreshold) {
-                    targetPage = currentPage - 1;
-                }
-            }
-
-            if (
-                targetPage !== currentPage &&
-                targetPage >= 0 &&
-                targetPage <= 1
-            ) {
-                isScrolling = true;
-                window.scrollTo({
-                    top: targetPage * pageHeight,
-                    behavior: 'smooth',
-                });
-
-                setTimeout(() => {
-                    isScrolling = false;
-                }, 800);
-            }
-        };
-
-        window.addEventListener('wheel', handleWheel, { passive: false });
-        return () => window.removeEventListener('wheel', handleWheel);
-    }, []);
-
+    usePageSnapScroll({ maxPage: 1, upDurationMs: 200, downDurationMs: 1400 });
     return (
         <div>
             <ForecastBackgroundSection />
